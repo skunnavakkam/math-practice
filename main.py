799a76e6d8cb9c4b5ed1b376d33d0ee5cbcca52f
skunnavakkam <73597280+skunnavakkam@users.noreply.github.com>
2022-08-03 23:08:46 -0700

    starting to add firebase

--- main.py
+++ main.py
@@ -2,22 +2,35 @@
 from flask import Flask, render_template
 import sqlite3
 import random
+import pyrebase
 
-def get_data(code):
-    loc = {}
-    exec(code, globals(), loc)
-    return loc['data']
+## FIREBASE STUFF ##
+config = {
+    "apiKey": "AIzaSyDpcvFe3H4uYwyHHwz1WEdP5GxWYUFCXPo",
+    "authDomain": "math-practice-6b4cb.firebaseapp.com",
+    "projectId": "math-practice-6b4cb",
+    "storageBucket": "math-practice-6b4cb.appspot.com",
+    "messagingSenderId": "626197126343",
+    "appId": "1:626197126343:web:3c8f36ca9373083c1f72e2",
+    "measurementId": "G-5FD202CJ27"
+}
+
+firebase = pyrebase.initialize_app(config)  
+auth = firebase.auth()
 
 app = Flask(__name__)
 
+# home, the pretty shit
 @app.route("/")
 def home():
     return render_template("home.html")
 
+# overarching topics page
 @app.route("/topics")
 def topics():
     return render_template("topics.html")
 
+# a way to navigate within topics
 @app.route("/topic-overview/<topic>")
 def topic_overview(topic):
     
@@ -28,6 +41,7 @@ def topic_overview(topic):
     problems = [[i[1], i[2]] for i in temp]
     return render_template("topic-overview.html", topic=topic.title(), problems=problems)
 
+# gives you the problem
 @app.route("/problems/<name>")
 def problems(name):
     
@@ -48,4 +62,10 @@ def problems(name):
         return render_template('text-question.html', question=data['question'], answer=data['answer'], is_calc=data['is_calc'])
     
     
-    
+########################################
+####        HELPER FUNCTIONS        ####
+########################################
+def get_data(code):
+    loc = {}
+    exec(code, globals(), loc)
+    return loc['data']
--- test.py
+++
@@ -1,8 +0,0 @@
-
-import sqlite3
-
-con = sqlite3.connect("problems.db")
-
-cur = con.cursor()
-
-cur.execute()
